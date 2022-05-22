8a1bc36519ba167dda17b0596c383d1f32986644
Shehzad <shehzaddiqbal@gmail.com>
2022-05-21 17:13:15 -0700

    hoverrr!!!!

---
+++ index.js
@@ -0,0 +1,9 @@
+//Show Password funciton
+function myFunction() {
+	var x = document.getElementById("password");
+	if (x.type === "password") {
+	  x.type = "text";
+	} else {
+	  x.type = "password";
+	}
+  }
