55addadaf32f13092e8480e3fd66bc788d7b8aae
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-09-26 02:02:37 +0500

    Formik working Show password done

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,8 +1,32 @@
 // import { BrowserRouter ,Routes,Route,Link } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useFormik } from 'formik';
 
+
+const initialValues={
+	email:"",
+	password:"",
+};
+
+const show_password = () => {
+	const pass1 = document.getElementById("password");
+	if (pass1.type === "password") pass1.type = "text";
+	else pass1.type = "password";
+}
 
 const Login = () => {
+
+
+	const {values, errors, handleBlur, handleChange ,handleSubmit} = useFormik({
+		initialValues: initialValues,
+		onSubmit:(values)=>{
+			console.log(values)
+		}
+	})
+	// console.log(Formik)
+
+
+
 	return (
 		<div className="none">
 
@@ -12,7 +36,7 @@ const Login = () => {
 			<button><Link to="/signup">SignUp</Link></button>
 
 
-			<form onsubmit="form_valadtion(); return false" className="form">
+			<form  className="form" onSubmit={handleSubmit}>
 
 				<div className="title">Welcome Back Login</div>
 				<div className="subtitle">shehheh!</div>
@@ -20,24 +44,24 @@ const Login = () => {
 
 
 				<div className="input-container ic2">
-					<input className="input" type="email" placeholder=" " name="email" required />
+					<input className="input" id="email" type="email" placeholder=" " name="email" required autoComplete="off" value={values.email} onChange={handleChange} onBlur={handleBlur}/>
 					<div className="cut"></div>
-					<label for="Email" className="placeholder">Email</label>
+					<label htmlFor="email" className="placeholder">Email</label>
 				</div>
 				<div className="input-container ic2">
-					<input id="password" className="input" type="password" placeholder=" " name="password" required />
+					<input id="password" className="input" type="password" placeholder=" " name="password" required autoComplete="off" value={values.password}  onChange={handleChange} onBlur={handleBlur}/>
 					<div className="cut cut-short"></div>
-					<label for="Password" className="placeholder">Password </label>
+					<label htmlFor="password" className="placeholder">Password </label>
 				</div>
 				<br />
-				<input type="checkbox" onclick="show_password()" /><span className="showPassword">Show Password</span>
+				<input type="checkbox" onClick={show_password} /><span className="showPassword">Show Password</span>
 				<br /><br />
 
 
 				<p id="error_msg"></p>
 
 				<div className="mainDiv">
-					<button type="text" className="submit secondBTN">SUBMIT </button>
+					<button type="submit" className="submit secondBTN">SUBMIT </button>
 				</div>
 				<br />
 
