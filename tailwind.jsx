2540c3fef0a1a5d6ea8b1b12b09da5b1b9ef6159
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-03-03 22:39:56 +0500

    tailwind form
    https://tailwindcss.com/docs/hover-focus-and-other-states#styling-based-on-parent-state

---
+++ tailwind.jsx
@@ -0,0 +1,13 @@
+<form>
+  <label class="block">
+    <span class="block text-sm font-medium text-slate-700">Username</span>
+    <!-- Using form state modifiers, the classes can be identical for every input -->
+    <input type="text" value="tbone" disabled class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "/>
+  </label>
+  <!--... -->
+</form>
